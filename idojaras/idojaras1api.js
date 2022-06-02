a9413a39407d5f94dbc6e00ae996de67799a6578
KOVDOM <94444372+KOVDOM@users.noreply.github.com>
2022-06-02 16:20:45 +0200

    Ezek a jók

--- idojaras/idojaras1api.js
+++ idojaras/idojaras1api.js
@@ -1,8 +1,10 @@
 var state;
+var main = document.getElementById('#name');
+var temp = document.getElementById('.temp');
+var desc = document.getElementById('.desc');
+var clouds = document.getElementById('.clouds');
 
-function lekerdez() {
-
-  
+function lekerdez(name) {  
     fetch('http://api.openweathermap.org/data/2.5/weather?q=Miskolc&appid=15d17ce5384d5d7a43a97af40fc78181&units=metric&lang=hu', {
       method: 'GET'
     })
@@ -13,45 +15,20 @@ function lekerdez() {
         }
         return response.json();
       })
-      .then(function (temp) {
-        console.log(temp);
-      console.log(temp.data);
-      render();
-      //document.getElementById('login').style.display="none";
+      .then(data => { 
+      var tempValue = data['main']['temp'];
+      var nameValue = data['name'];
+      var descValue = data['weather'][0]['description'];
+    
+      document.getElementById('main').innerHTML = nameValue;
+      document.getElementById('desc').innerHTML = "Desc - "+descValue;
+      document.getElementById('temp').innerHTML = "Temp - "+tempValue;
+        console.log(tempValue);
+      console.log(nameValue);
+      console.log(descValue);
       })
       .catch(function (error) {
         console.log(error);
         alert(error);
       })
-
   };
-  
-  function render(){
-    console.log(state);
-    var tmp=`<p>Hőmérséklet: +${state}</p>`;
-  
-    for(var u of state.list){
-      tmp+=`<p>Hőmérséklet: +${state}</p>`;
-    }
-    document.getElementById('megjelenit').innerHTML=tmp+`</p>`;
-  }
-  //<li class="list-group-item">${unknown.name}</li>
-  
-  function renderUnknownId(){
-    console.log(state.id);
-    if(state.id!=""){
-      var tmp=`<table class='table'><tr><th scope='col'>
-    ID</th><th scope='col'>
-    Name</th><th scope='col'>Year</th><th scope='col'>
-    Color</th> <th scope="col">
-    Pantone_value</th></tr>`;  
-    tmp+=`<tr>
-      <td>${state.id.id}</td>
-      <td>${state.id.name}</td>
-      <td>${state.id.year}</td>
-      <td style="background-color:${state.id.color}">${state.id.color}</td>
-      <td>${state.id.pantone_value}</td>    
-      </tr>`;
-      document.getElementById('unknownid').innerHTML=tmp+`</table>`;
-    }
-  }
